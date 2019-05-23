0049625191b2b87d425a40fc0bb9efed5218e8c8
baotuane <letuanbao1993@gmail.com>
2019-05-23 15:48:23 +0700

    guitincanbang

---
+++ app/src/main/java/develop/admin/it/formular/CaiDatChuyenTinActivity.java
@@ -0,0 +1,13 @@
+package develop.admin.it.formular;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CaiDatChuyenTinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_cai_dat_chuyen_tin );
+    }
+}
